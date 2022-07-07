e2e5866ff7a808ddc2ada0dec68f2e1de8163346
Anany Prakhar <vibhu.prakhar@gmail.com>
2022-07-07 12:49:44 +0530

    Feat: Add progress indicator

--- rankSelectMatchv2.cpp
+++ rankSelectMatchv2.cpp
@@ -33,11 +33,14 @@ ll getMileStoneRankFromRankDS(char, ll, vector<vector<ull>>&);
 ll getBandInFirstColFromRank(char, ll, vector<ull>&);
 void getPosInRef(ll, ll, vector<vector<ull>>&, vector<ull>&, vector<int>&, string, fstream&, vector<ll>&);
 char getLastCharAtRow(ll,  vector<int>&);
+ll getFileSize(fstream&);
 
 int main(int argc, char const *argv[]){
     fstream readsFile(readsFilename, ios::in);
     fstream readsMatchIndexFile(readsMatchIndexFilename, ios::out | ios::trunc);
     string read, complementReversed;
+    ll readsFileSize;
+    vector<string> readsAndRevComplReads;
     ////////////
     fstream milestoneMapFile(milestoneMapFilename, ios::in);
 
@@ -66,52 +69,69 @@ int main(int argc, char const *argv[]){
     vector<ull> firstCol(getFirstCol());
     vector<vector<ull>> ACGT_last_rank(getACGTLastRank());
     vector<int> lastColBitMap(getLastColBitMap());
+    readsFileSize = getFileSize(readsFile);
     cout<<"Read into memory completed.\n";
 
-    cout<<"Reading read strings.\n";
     while(getline(readsFile, read)){
         // cout<<"Found "<<read<<"? ";
-
+        // cout<<"\rProgress: "<<(double)readsFile.tellg()*100/readsFileSize<<"%"<<flush;
         
         read = cleanupReadString(read);
-        complementReversed = makeReversedComplement(read); 
-        rankSelectInit(read, ACGT_last_rank, lastColBitMap, firstCol,readsMatchIndexFile, milestoneMap);  
-        rankSelectInit(complementReversed, ACGT_last_rank, lastColBitMap, firstCol, readsMatchIndexFile, milestoneMap);  
-        // ll band_start, band_end;
-        // char lastChar = read.back();
-        // bool found = false;
-
-        // read = cleanupReadString(read);
-        // read_complement = makeReversedComplement(read);
-        // readStrLen = read.size();
-        // read.pop_back();
-
-        // /*
-        //     A   A   C   C   G   G   T
-        //     0   1   2   3   4   5   6
-        //     firstCol[A] = 2
-        //     firstCol[C] = 2
-        //     firstCol[G] = 2
-        //     firstCol[T] = 1
-        // */
-        // ll A_start, C_start, G_start, T_start, A_end, C_end, G_end, T_end;
-        // A_start = 0; C_start = A_start + firstCol[0]; G_start = C_start + firstCol[1]; T_start = G_start + firstCol[2];
-        // A_end = firstCol[0] -1; C_end = A_end + firstCol[1]; G_end = C_end + firstCol[2]; T_end = G_end + firstCol[3];
-
-        // switch(lastChar) {
-        //     case 'A': band_start = A_start; band_end = A_end; break;
-        //     case 'C': band_start = C_start; band_end = C_end; break;
-        //     case 'G': band_start = G_start; band_end = G_end; break;
-        //     case 'T': band_start = T_start; band_end = T_end; break;
-        //     default: cout<<"Invalid string. Found unknown character: "<<lastChar<<".\n"; exit(EXIT_FAILURE);
-        // }
-        // found = rankSelect(read, band_start, band_end, ACGT_last_rank, lastColBitMap, firstCol);
-        // if (found) {
-        //     cout<<"BS:"<<band_start<<", BE:"<<band_end<<".\n";
-        //     getPosInRef(band_start, band_end,ACGT_last_rank, firstCol, lastColBitMap);
-        // } else cout<<"Cannot find this read string.\n";
-        // cout<<endl;
+        readsAndRevComplReads.push_back(read);
+        complementReversed = makeReversedComplement(read);
+        readsAndRevComplReads.push_back(complementReversed);
     }
+    cout<<"Reading read strings.\n";
+    ll readsArraySize = readsAndRevComplReads.size();
+    for (ll i = 0; i < readsArraySize; i++){
+        cout<<"\rProgress: "<<(double)i*100/readsArraySize<<"%"<<flush;
+        rankSelectInit(readsAndRevComplReads[i], ACGT_last_rank, lastColBitMap, firstCol,readsMatchIndexFile, milestoneMap);  
+    }
+    
+    // while(getline(readsFile, read)){
+    //     // cout<<"Found "<<read<<"? ";
+    //     cout<<"\rProgress: "<<(double)readsFile.tellg()*100/readsFileSize<<"%"<<flush;
+        
+    //     read = cleanupReadString(read);
+    //     complementReversed = makeReversedComplement(read); 
+    //     rankSelectInit(read, ACGT_last_rank, lastColBitMap, firstCol,readsMatchIndexFile, milestoneMap);  
+    //     rankSelectInit(complementReversed, ACGT_last_rank, lastColBitMap, firstCol, readsMatchIndexFile, milestoneMap);  
+    //     // ll band_start, band_end;
+    //     // char lastChar = read.back();
+    //     // bool found = false;
+
+    //     // read = cleanupReadString(read);
+    //     // read_complement = makeReversedComplement(read);
+    //     // readStrLen = read.size();
+    //     // read.pop_back();
+
+    //     // /*
+    //     //     A   A   C   C   G   G   T
+    //     //     0   1   2   3   4   5   6
+    //     //     firstCol[A] = 2
+    //     //     firstCol[C] = 2
+    //     //     firstCol[G] = 2
+    //     //     firstCol[T] = 1
+    //     // */
+    //     // ll A_start, C_start, G_start, T_start, A_end, C_end, G_end, T_end;
+    //     // A_start = 0; C_start = A_start + firstCol[0]; G_start = C_start + firstCol[1]; T_start = G_start + firstCol[2];
+    //     // A_end = firstCol[0] -1; C_end = A_end + firstCol[1]; G_end = C_end + firstCol[2]; T_end = G_end + firstCol[3];
+
+    //     // switch(lastChar) {
+    //     //     case 'A': band_start = A_start; band_end = A_end; break;
+    //     //     case 'C': band_start = C_start; band_end = C_end; break;
+    //     //     case 'G': band_start = G_start; band_end = G_end; break;
+    //     //     case 'T': band_start = T_start; band_end = T_end; break;
+    //     //     default: cout<<"Invalid string. Found unknown character: "<<lastChar<<".\n"; exit(EXIT_FAILURE);
+    //     // }
+    //     // found = rankSelect(read, band_start, band_end, ACGT_last_rank, lastColBitMap, firstCol);
+    //     // if (found) {
+    //     //     cout<<"BS:"<<band_start<<", BE:"<<band_end<<".\n";
+    //     //     getPosInRef(band_start, band_end,ACGT_last_rank, firstCol, lastColBitMap);
+    //     // } else cout<<"Cannot find this read string.\n";
+    //     // cout<<endl;
+    // }
+    cout<<"\n";
 
     cout<<endl;   
 }
@@ -255,8 +275,18 @@ bool rankSelect(string& read, ll& band_start, ll& band_end, vector<vector<ull>>&
     ll rank_first_ch, rank_last_ch, index_nearestMilestone_bitmap, index_nearestMilestone_rankDS, count_ch_fromMilestone_toBandStart = 0, count_ch_fromMileStone_toBandEnd = 0;
     index_nearestMilestone_rankDS = band_start/DELTA; // index of nearest milstone in rank DS (rankDS = rank data structure)
     index_nearestMilestone_bitmap = band_start - (band_start%DELTA); // index of nearest milstone in bitmap array
-    for (ll i = index_nearestMilestone_bitmap; i < band_start; i++){
-        if(lastColBitMap[i] == charToBitStr(ch)) count_ch_fromMilestone_toBandStart++; // counts until just before band_start char (see definition of rank).
+    // for (ll i = index_nearestMilestone_bitmap; i < band_start; i++){
+    //     if(lastColBitMap[i] == charToBitStr(ch)) count_ch_fromMilestone_toBandStart++; // counts until just before band_start char (see definition of rank).
+    // }
+
+    // get number of ch in between index_nearestMilestone_bitmap to the first occurence of ch in the band (index_nearestMilestone_bitmap inclusive only)
+    ll iter = index_nearestMilestone_bitmap;
+    while(true) {
+        if (iter >= band_end) break;
+        if (iter >= band_start && lastColBitMap[iter] == charToBitStr(ch)) break;
+        if (lastColBitMap[iter] == charToBitStr(ch)) count_ch_fromMilestone_toBandStart++;
+
+        iter++;
     }
     // Use this logic 1 or 2
     /*
@@ -268,9 +298,19 @@ bool rankSelect(string& read, ll& band_start, ll& band_end, vector<vector<ull>>&
     if(count_ch_fromMileStone_toBandEnd < 0) return false; // check this condition against test cases
     */
     // Logic 2
-    for (ll i = index_nearestMilestone_bitmap; i < band_end; i++){
-        if(lastColBitMap[i] == charToBitStr(ch)) count_ch_fromMileStone_toBandEnd++; // counts until just before band_end char (see definition of rank).
+    // for (ll i = index_nearestMilestone_bitmap; i < band_end; i++){
+    //     if(lastColBitMap[i] == charToBitStr(ch)) count_ch_fromMileStone_toBandEnd++; // counts until just before band_end char (see definition of rank).
+    // }
+
+    // get number of ch in between index_nearestMilestone_bitmap to the last occurence of ch in the band (index_nearestMilestone_bitmap inclusive only)
+    iter = index_nearestMilestone_bitmap;
+    while(true) {
+        if (iter > band_end) break;
+        if (lastColBitMap[iter] == charToBitStr(ch)) count_ch_fromMileStone_toBandEnd++;
+        iter++;
     }
+    count_ch_fromMileStone_toBandEnd--; // remove the last ch that got counted.
+    
     // if(count_ch_fromMileStone_toBandEnd - count_ch_fromMilestone_toBandStart < 0) return false;
     rank_first_ch = getMileStoneRankFromRankDS(ch, index_nearestMilestone_rankDS, ACGT_last_rank) + count_ch_fromMilestone_toBandStart;
     rank_last_ch = getMileStoneRankFromRankDS(ch, index_nearestMilestone_rankDS, ACGT_last_rank) + count_ch_fromMileStone_toBandEnd;
@@ -295,7 +335,7 @@ bool rankSelect(string& read, ll& band_start, ll& band_end, vector<vector<ull>>&
     // <<",band_end:"<<band_end
     // <<"]\n"
     // ;
-
+    
     return rankSelect(read, band_start, band_end, ACGT_last_rank, lastColBitMap, firstCol);
 }
 
@@ -390,10 +430,11 @@ void getPosInRef(ll band_start, ll band_end, vector<vector<ull>>& ACGT_last_rank
         // cout<<"\tStart position in ref string is at: "<<milestoneMap[band]+count_jumps<<". (count_jumps="<<count_jumps<<", milestoneMap[band]"<<milestoneMap[band]<<", band:"<<band<<")\n";
         startIndexInRef = milestoneMap[(ll)(band/DELTA)]+count_jumps;
         // cout<<"Start position in ref string is at: "<<startIndexInRef<<"\n";
+        readsMatchIndexFile<<startIndexInRef<<" ["<<read<<"]\n"<<flush;
         count_jumps = 0;
     }
 
-    readsMatchIndexFile<<startIndexInRef<<" ["<<read<<"]\n";
+    // readsMatchIndexFile<<startIndexInRef<<" ["<<read<<"]\n";
 }
 
 char getLastCharAtRow(ll index, vector<int>& lastColBitMap) {
@@ -406,4 +447,12 @@ char getLastCharAtRow(ll index, vector<int>& lastColBitMap) {
         default: cout<<"Invalid bitstr from lastColBitMap.\n"; exit(EXIT_FAILURE);
     };
     return 'X';
+}
+
+ll getFileSize(fstream& file){
+    ll currPos = file.tellg();
+    file.seekg(0, ios::end);
+    ll size = file.tellg();
+    file.seekg(currPos, ios::beg);
+    return size;
 }
